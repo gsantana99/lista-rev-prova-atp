09daa00665604abd7bb3d2fff4909ff5cc59dfcb
Gabriel Santana Barroso <gsbarroso@sga.pucminas.br>
2019-05-08 20:05:04 -0300

    Revert ""
    This reverts commit d8b856f20637e1ee9ae2398fa1ce60ba0cd19f7d.

--- Exercicio9.java
+++ Exercicio9.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Exercicio9 {
 
     public static void maiorEmenor() {
-        System.out.println("Insira 5 números separados para que sejam determinados o maior e o menor: ");
+        System.out.println("Insira 5 números separados por espaços para que sejam determinados o maior e o menor: ");
         Scanner scanner = new Scanner(System.in);
         int maior = 0;
         int menor = 0;
--- Main.java
+++ Main.java
@@ -10,15 +10,12 @@ public class Main {
         char sexo;
         double raio, altura, mediaArit;
 
-        System.out.println("Questão 1:");
-        System.out.print("Insira um número para que todos os termos entre 1 e ele sejam somados: ");
+      /*  System.out.print("Insira um número para que todos os termos entre 1 e ele sejam somados: ");
         n = scanner.nextInt();
         int soma_ate_n = Exercicio1.soma_ate_n(n);
-        System.out.println("Resultado da soma: " + soma_ate_n);
-        System.out.println();
+        System.out.print("Resultado da soma: " + soma_ate_n);
 
-        System.out.println("Questão 2:");
-        System.out.print("Insira um número de horas para ser convertido em segundos: ");
+        System.out.print("\n\nInsira um número de horas para ser convertido em segundos: ");
         n_horas = scanner.nextInt();
         System.out.print("Insira um número de minutos para ser convertido em segundos: ");
         n_minutos = scanner.nextInt();
@@ -26,32 +23,24 @@ public class Main {
         n_segundos = scanner.nextInt();
         int t_segundos = Exercicio2.converte_segundos(n_horas, n_minutos, n_segundos);
         System.out.println("Resultado da soma (H + M + S) em segundos: " + t_segundos);
-        System.out.println();
 
-        System.out.println("Questão 3:");
         System.out.println("Insira a primeira string a ser comparada: ");
         comparavel1 = scanner.next();
         System.out.println("Insira a segunda string a ser comparada: ");
         comparavel2= scanner.next();
         int resultado_cstr = Exercicio3.compara_strings(comparavel1, comparavel2);
         System.out.println("Resultado da comparação: " + resultado_cstr);
-        System.out.println();
 
-        System.out.println("Questão 4:");
         System.out.println("Insira um valor para o raio de uma esfera para que seu volume seja calculado: ");
         raio = scanner.nextDouble();
         double vol_esfera = Exercicio4.calcVolEsfera(raio);
         System.out.println("Volume da esfera: " + vol_esfera);
-        System.out.println();
 
-        System.out.println("Questão 5:");
         System.out.println("Insira um número para que seja verificado se ele é positivo ou negativo: ");
         num = scanner.nextInt();
         String sinal_num = Exercicio5.ehPositivo(num) ? "é positivo" : "é negativo";
         System.out.println(sinal_num);
-        System.out.println();
 
-        System.out.println("Questão 6:");
         System.out.println("Informe o sexo da pessoa (f/m): ");
         sexo = scanner.next().charAt(0);
         System.out.println("Informe a altura da pessoa: ");
@@ -62,18 +51,14 @@ public class Main {
         } else {
             System.out.println("O peso ideal desta pessoa é: " + pesoIdeal);
         }
-        System.out.println();
 
-        System.out.println("Questão 7:");
         n_valores = new Random().nextInt(10) + 1;
         System.out.println("Insira " + n_valores + " separados por espaço para que a média aritmética deles seja calculada: ");
         mediaArit = Exercicio7.calcMediaArit();
         System.out.println("A média aritmética dos números inseridos é: " + mediaArit);
-        System.out.println();
 
-        System.out.println("Questão 9:");
-        Exercicio9.maiorEmenor();
-        System.out.println();
+        Exercicio9.maiorEmenor();*/
+
 
 
         /* = Exercicio10;
